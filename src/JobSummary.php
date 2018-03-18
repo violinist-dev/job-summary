4e1860b56a2011f1b3c8ff29d4adc4e823c4c87c
Eirik S. Morland <eirik@morland.no>
2018-03-18 17:49:44 +0100

    All of this

--- src/JobSummary.php
+++ src/JobSummary.php
@@ -1,8 +1,163 @@
 <?php
 
-namespace violinist/JobSummary;
+namespace violinist\JobSummary;
 
 class JobSummary
 {
+
+    protected $rawMessages;
+
+    protected $errors = [];
+
+    protected $outsideConstraint = [];
+
+    protected $updates = [];
+
+    protected $notUpdated = [];
+
+    protected $prs = [];
+
+    protected $blacklisted = [];
+
+  /**
+   * @return array
+   */
+  public function getPrs() {
+    return $this->prs;
+  }
+
+  /**
+   * @param array $prs
+   */
+  public function setPrs($prs) {
+    $this->prs = $prs;
+  }
+
+  /**
+   * @return array
+   */
+  public function getBlacklisted() {
+    return $this->blacklisted;
+  }
+
+  /**
+   * @param array $blacklisted
+   */
+  public function setBlacklisted($blacklisted) {
+    $this->blacklisted = $blacklisted;
+  }
+
+  /**
+   * @return array
+   */
+  public function getNotUpdated() {
+    return $this->notUpdated;
+  }
+
+  /**
+   * @param array $notUpdated
+   */
+  public function setNotUpdated($notUpdated) {
+    $this->notUpdated = $notUpdated;
+  }
+
+  /**
+   * @return array
+   */
+  public function getUpdates() {
+    return $this->updates;
+  }
+
+  /**
+   * @param array $updates
+   */
+  public function setUpdates($updates) {
+    $this->updates = $updates;
+  }
+
+  /**
+   * @return array
+   */
+  public function getOutsideConstraint() {
+    return $this->outsideConstraint;
+  }
+
+  /**
+   * @param array $outsideConstraint
+   */
+  public function setOutsideConstraint($outsideConstraint) {
+    $this->outsideConstraint = $outsideConstraint;
+  }
+
+  /**
+   * @return array
+   */
+  public function getErrors() {
+    return $this->errors;
+  }
+
+  /**
+   * @param array $errors
+   */
+  public function setErrors($errors) {
+    $this->errors = $errors;
+  }
+
+    public function __construct(array $messages)
+    {
+        $this->rawMessages = $messages;
+        $this->analyzeMessages();
+    }
+
+    protected function analyzeMessages()
+    {
+        foreach ($this->rawMessages as $message) {
+          if (is_string($message)) {
+              // This message is probably a horrible error.
+              $this->errors[] = $message;
+              continue;
+          }
+            switch ($message->type) {
+              case 'unupdate':
+                $this->outsideConstraint[] = $message;
+                break;
+
+              case 'notupdated':
+                $this->notUpdated[] = $message;
+                break;
+
+              case 'update':
+                if (empty($message->context)) {
+                  $this->updates = explode("\n", $message->message);
+                  // Remove the ones that are empty.
+                  foreach ($this->updates as $delta => $update) {
+                    if (empty($update)) {
+                      unset($this->updates[$delta]);
+                    }
+                  }
+                }
+                else {
+                  $this->updates = $message->context->packages;
+                }
+                break;
+
+              case 'pr_exists':
+                if (!empty($message->context)) {
+                  $this->prs[] = $message;
+                }
+                break;
+
+              case 'blacklisted':
+                if (!empty($message->context)) {
+                  $this->blacklisted[] = $message;
+                }
+                break;
+
+              default:
+                break;
+            }
+        }
+    }
+
 }
 
