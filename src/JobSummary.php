d018155f31a8403136101db8da9f3d8d06293066
Eirik S. Morland <eirik@morland.no>
2021-09-21 21:21:48 +0200

    Add tests and stuff

--- src/JobSummary.php
+++ src/JobSummary.php
@@ -28,95 +28,108 @@ class JobSummary
   /**
    * @return \stdClass[]
    */
-  public function getPrs() {
-    return $this->prs;
-  }
+    public function getPrs()
+    {
+        return $this->prs;
+    }
 
   /**
    * @param array $prs
    */
-  public function setPrs($prs) {
-    $this->prs = $prs;
-  }
+    public function setPrs($prs)
+    {
+        $this->prs = $prs;
+    }
 
   /**
    * @return array
    */
-  public function getBlacklisted() {
-    return $this->blacklisted;
-  }
+    public function getBlacklisted()
+    {
+        return $this->blacklisted;
+    }
 
   /**
    * @param array $blacklisted
    */
-  public function setBlacklisted($blacklisted) {
-    $this->blacklisted = $blacklisted;
-  }
+    public function setBlacklisted($blacklisted)
+    {
+        $this->blacklisted = $blacklisted;
+    }
 
   /**
    * @return \stdClass[]
    */
-  public function getNotUpdated() {
-    return $this->notUpdated;
-  }
+    public function getNotUpdated()
+    {
+        return $this->notUpdated;
+    }
 
   /**
    * @param array $notUpdated
    */
-  public function setNotUpdated($notUpdated) {
-    $this->notUpdated = $notUpdated;
-  }
+    public function setNotUpdated($notUpdated)
+    {
+        $this->notUpdated = $notUpdated;
+    }
 
   /**
    * @return array
    */
-  public function getUpdates() {
-    return $this->updates;
-  }
+    public function getUpdates()
+    {
+        return $this->updates;
+    }
 
   /**
    * @param array $updates
    */
-  public function setUpdates($updates) {
-    $this->updates = $updates;
-  }
+    public function setUpdates($updates)
+    {
+        $this->updates = $updates;
+    }
 
   /**
    * @return array
    */
-  public function getOutsideConstraint() {
-    return $this->outsideConstraint;
-  }
+    public function getOutsideConstraint()
+    {
+        return $this->outsideConstraint;
+    }
 
   /**
    * @param array $outsideConstraint
    */
-  public function setOutsideConstraint($outsideConstraint) {
-    $this->outsideConstraint = $outsideConstraint;
-  }
+    public function setOutsideConstraint($outsideConstraint)
+    {
+        $this->outsideConstraint = $outsideConstraint;
+    }
 
   /**
    * @return array
    */
-  public function getErrors() {
-    return $this->errors;
-  }
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 
   /**
    * @param array $errors
    */
-  public function setErrors($errors) {
-    $this->errors = $errors;
-  }
-
-  public function getUpdateForPackage($package) {
-    foreach ($this->getUpdates() as $update) {
-      if ($update->name == $package) {
-        return $update;
-      }
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
     }
-    return FALSE;
-  }
+
+    public function getUpdateForPackage($package)
+    {
+        foreach ($this->getUpdates() as $update) {
+            if ($update->name == $package) {
+                return $update;
+            }
+        }
+        return false;
+    }
 
     public function __construct(array $messages)
     {
@@ -140,83 +153,80 @@ class JobSummary
     protected function analyzeMessages()
     {
         foreach ($this->rawMessages as $message) {
-          if (is_string($message)) {
-              // This message is probably a horrible error.
-              $this->errors[] = $message;
-              continue;
-          }
-          if (!empty($message->message) && preg_match('/require.*should not contain uppercase/', $message->message, $output_array)) {
-              $this->composer2error = true;
-          }
-          if (!empty($message->context->package) && $message->type == 'command' && !empty($message->context->type) && $message->context->type === 'exit_code_output') {
-              // This means the package had an error upon update, and it was not
-              // updated.
-              $this->notUpdated[] = $message;
-              continue;
-          }
-          if (!empty($message->message) && $message->message === 'Cleaning up after update check.') {
-              // For now, let's say that indicates this was finished with
-              // success.
-              $this->finishedSuccessFully = true;
-          }
-            switch ($message->type) {
-              case 'unupdate':
-                $this->outsideConstraint[] = $message;
-                break;
-
-              case 'notupdated':
+            if (is_string($message)) {
+                // This message is probably a horrible error.
+                $this->errors[] = $message;
+                continue;
+            }
+            if (!empty($message->message) && preg_match('/require.*should not contain uppercase/', $message->message, $output_array)) {
+                $this->composer2error = true;
+            }
+            if (!empty($message->context->package) && $message->type == 'command' && !empty($message->context->type) && $message->context->type === 'exit_code_output') {
+                // This means the package had an error upon update, and it was not
+                // updated.
                 $this->notUpdated[] = $message;
-                break;
+                continue;
+            }
+            if (!empty($message->message) && $message->message === 'Cleaning up after update check.') {
+                // For now, let's say that indicates this was finished with
+                // success.
+                $this->finishedSuccessFully = true;
+            }
+            switch ($message->type) {
+                case 'unupdate':
+                    $this->outsideConstraint[] = $message;
+                    break;
 
-              case 'update':
-                if (empty($message->context)) {
-                  $this->updates = explode("\n", $message->message);
-                  // Remove the ones that are empty.
-                  foreach ($this->updates as $delta => $update) {
-                    if (empty($update)) {
-                      unset($this->updates[$delta]);
+                case 'notupdated':
+                    $this->notUpdated[] = $message;
+                    break;
+
+                case 'update':
+                    if (empty($message->context)) {
+                        $this->updates = explode("\n", $message->message);
+                      // Remove the ones that are empty.
+                        foreach ($this->updates as $delta => $update) {
+                            if (empty($update)) {
+                                unset($this->updates[$delta]);
+                            }
+                        }
+                    } else {
+                        $this->updates = [];
+                      // "Convert" to array.
+                        foreach ($message->context->packages as $package) {
+                            $this->updates[] = $package;
+                        }
                     }
-                  }
-                }
-                else {
-                  $this->updates = [];
-                  // "Convert" to array.
-                  foreach ($message->context->packages as $package) {
-                    $this->updates[] = $package;
-                  }
-                }
-                break;
+                    break;
 
-              case 'pr_url':
-                if (!empty($message->context)) {
-                  $this->prs[] = $message;
-                }
-                break;
+                case 'pr_url':
+                    if (!empty($message->context)) {
+                        $this->prs[] = $message;
+                    }
+                    break;
 
 
-              case 'pr_exists':
-                if (!empty($message->context)) {
-                  $this->prs[] = $message;
-                }
-                break;
+                case 'pr_exists':
+                    if (!empty($message->context)) {
+                        $this->prs[] = $message;
+                    }
+                    break;
 
-              case 'blacklisted':
-                if (!empty($message->context)) {
-                  $this->blacklisted[] = $message;
-                }
-                break;
+                case 'blacklisted':
+                    if (!empty($message->context)) {
+                        $this->blacklisted[] = $message;
+                    }
+                    break;
 
-              case 'error':
-                if (!empty($message->context->package)) {
-                  $this->errors[] = $message;
-                }
-                break;
+                case 'error':
+                    if (!empty($message->context->package)) {
+                        $this->errors[] = $message;
+                    }
+                    break;
 
-              default:
-                break;
+                default:
+                    break;
             }
         }
     }
-
 }
-
